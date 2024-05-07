43e03a426e2677bc9c9d2d45cca2d1e6ced08bf5
Khandaker Nabiul <na.ninad60@gmail.com>
2024-05-08 03:55:03 +0600

    Create FAQ page with relative component

---
+++ app/faq/page.tsx
@@ -0,0 +1,13 @@
+import { YouMayLike } from "@/components";
+import { FaqSection } from "@/components/Pages/Faq";
+
+const FaqPage = () => {
+    return(
+        <>
+            <FaqSection/>
+            <YouMayLike/>
+        </>
+    );
+};
+
+export default FaqPage;
